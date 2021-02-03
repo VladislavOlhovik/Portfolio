80695d544c3a69e08f1de5eedd8528ed10ed01ac
Vladislav Olhovik <alkhovikvlad@gmail.com>
2021-02-03 15:43:06 +0100

    add cv

--- src/OpenToOffers/OpenToOffers.jsx
+++ src/OpenToOffers/OpenToOffers.jsx
@@ -3,13 +3,14 @@ import styleContainer from '../common/styles/Container.module.css'
 import style from './OpenToOffers.module.scss'
 import Button from '../common/components/button/Button'
 import Title from '../common/components/title/Title';
+import myCV from '../assets/documents/Uladzislau Alkhovik-CV.pdf'
 
 function OpenToOffers() {
   return (
     <div className={style.remoteWork}>
       <div className={`${styleContainer.container} ${style.container}`}>
         <Title title={"I'm open to job offers"}/>
-        <Button text={'Hire Me'} url={'mailto:alkhovikvlad@gmail.com'} />
+        <Button text={'Download CV'} url={myCV}/>
       </div>
     </div>
   );
